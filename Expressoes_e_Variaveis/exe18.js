6b2e97a3771e6b1e6003e05a2b2b72ff8d155474
pedrorangeljr <pedro.curtis.rangel@gmail.com>
2024-06-16 15:46:59 -0300

    exe18 ao exe20

---
+++ Expressoes_e_Variaveis/exe18.js
@@ -0,0 +1,7 @@
+let prompt = require('prompt-sync')();
+
+let M = prompt("");
+
+let L = 1000 * M;
+
+console.log(`O volume em Litros: ${M}`);
---
+++ Expressoes_e_Variaveis/exe19.js
@@ -0,0 +1,7 @@
+let prompt = require('prompt-sync')();
+
+let L = prompt("");
+
+let M = L/1000;
+
+console.log(`O volume em Metros cúbicos: ${M}`);
